b1722cf9f29d552524d9516b2a7dd2fb6bb5b626
Montanaro02 <100145766+Montanaro02@users.noreply.github.com>
2022-05-07 17:05:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char l;
+    cin >> l;
+    if ((l > 64 && l < 91) || (l > 96 && l < 123)) {
+        cout << "vero\n";
+        if ((l >= 64 && l < 91)) {
+            l = l + 32;
+            cout << l;
+        } else if ((l > 96 && l < 123)) {
+            l = l - 32;
+            cout << l;
+        } else {
+            cout << "Errore";
+        }
+        return 0;
+    }
 }
