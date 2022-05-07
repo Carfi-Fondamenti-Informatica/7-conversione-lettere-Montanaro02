6fb66e8775895e3b1b1c8fdaf0ae281a2ae13ade
Montanaro02 <100145766+Montanaro02@users.noreply.github.com>
2022-05-07 17:56:12 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,6 +1,3 @@
-//
-// Created by loren on 12/04/2022.
-//
 #include "lib.h"
 bool verifica(char l) {
     if((l>=65 && l<=90) || (l>=90 && l<=122)){
@@ -13,8 +10,9 @@ char conversione (char l) {
     char operazione;
     if ((l >= 64 && l < 91)) {
         operazione = l + 32;
+        return operazione;
     } else if ((l > 96 && l < 123)) {
         operazione = l - 32;
+        return operazione;
     }
-    return operazione;
 }
